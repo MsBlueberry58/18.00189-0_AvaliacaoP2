2811c9b995c59dd2b3af45ab93fd893733f0df67
MsBlueberry58 <brunagguedes4@gmail.com>
2020-12-01 16:33:19 -0300

    Ajustes finais

--- flutter_p2/lib/telas/tela4.dart
+++ flutter_p2/lib/telas/tela4.dart
@@ -1,10 +1,40 @@
+import 'dart:math';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_p2/models/pokemon.dart';
 import 'package:flutter_p2/telas/tela1.dart';
 import 'package:flutter_p2/telas/tela2.dart';
 import 'package:flutter_p2/telas/tela3.dart';
+import 'package:flutter_p2/utilities/network_helper.dart';
 
 class Tela4 extends StatelessWidget {
+
+  Random random = new Random();
+  Pokemon_Obj poke_t = new Pokemon_Obj();
+  Pokemon_Obj poke_1 = new Pokemon_Obj();
+  Pokemon_Obj poke_2 = new Pokemon_Obj();
+  Pokemon_Obj poke_3 = new Pokemon_Obj();
+
+
+  Future FetchPoke()  async {
+    var requisicao =
+    NetworkHelper(url: "https://pokeapi.co/api/v2/pokemon-form/${random.nextInt(895)}");
+    poke_t = Pokemon_Obj.fromJson(await requisicao.getData());
+    var requisicao2 =
+    NetworkHelper(url: "https://pokeapi.co/api/v2/pokemon-form/${random.nextInt(895)}");
+    poke_1 = Pokemon_Obj.fromJson(await requisicao2.getData());
+    var requisicao3 =
+    NetworkHelper(url: "https://pokeapi.co/api/v2/pokemon-form/${random.nextInt(895)}");
+    poke_2 = Pokemon_Obj.fromJson(await requisicao3.getData());
+    var requisicao4 =
+    NetworkHelper(url: "https://pokeapi.co/api/v2/pokemon-form/${random.nextInt(895)}");
+    poke_3 = Pokemon_Obj.fromJson(await requisicao4.getData());
+
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -60,10 +90,11 @@ class Tela4 extends StatelessWidget {
                 color: Colors.black,
               ),
             ),
-            onPressed: () {
+            onPressed: () async {
+              await FetchPoke();
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => Tela1()),
+                MaterialPageRoute(builder: (context) =>  Tela2(poke: poke_t, poke1: poke_1, poke2: poke_2, poke3: poke_3)),
               );
             },
           ),)
